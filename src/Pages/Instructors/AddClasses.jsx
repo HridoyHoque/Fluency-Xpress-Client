8be97db6251451e5a61fa32d0a50f8ad4ddb0587
HridoyHoque <hhridoy155@gmail.com>
2023-06-11 23:11:06 +0600

    added post method to post newClasses

--- src/Pages/Instructors/AddClasses.jsx
+++ src/Pages/Instructors/AddClasses.jsx
@@ -1,14 +1,40 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
-
+import toast, { Toaster } from 'react-hot-toast';
 
 
 const AddClasses = () => {
 const {user} = useContext(AuthContext)
 
-    const handleAddClass = () => {
-        console.log()
+    const handleAddClass = (event) => {
+       event.preventDefault()
+
+       const form = event.target;
+       const instructorName = form.instructorName.value;
+       const email = form.email.value;
+       const image = form.image.value;
+       const name = form.name.value;
+       const price = form.price.value;
+       const seats = form.seats.value;
+       const status = form.status.value;
+       const newClass = {instructorName, email, image, name, price, seats, status}
+       console.log(newClass)
+
+       fetch('http://localhost:5000/newClasses',{
+        method: "POST",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(newClass)
+       })
+       .then(res => res.json())
+       .then(data => {
+        console.log(data);
+        if(data.insertedId){
+            toast.success('Class added successfully')
+        }
+       })
     } 
 
     return (
@@ -32,7 +58,7 @@ const {user} = useContext(AuthContext)
                     <label className="label">
                         <span className="label-text">Class Image URL</span>
                     </label>
-                    <input type="text" required name="photo" placeholder='Class Image' className="input input-bordered" />
+                    <input type="text" required name="image" placeholder='Class Image' className="input input-bordered" />
                 </div>
                 <div className="form-control">
                     <label className="label">
@@ -62,6 +88,7 @@ const {user} = useContext(AuthContext)
             <div className="form-control mt-6">
                 <input className="btn btn-primary btn-block" type="submit" value="Add Class" />
             </div>
+            <Toaster />
         </form>
     );
 };
